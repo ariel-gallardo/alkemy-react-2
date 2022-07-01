af4b9150852ffa5059656cd540a5fa9e811db2e3
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-01 18:26:14 -0300

    main# essential component for dev

---
+++ src/Components/Content.jsx
@@ -0,0 +1,15 @@
+import './Styles/App.css'
+
+const Content = ({children}) => <>
+    <div className='app'>
+    <div className='navigation'/>
+      <div className='content'>
+        <div className='centerContent'>
+          {children}
+        </div>
+      </div>
+      <div className='footer'/>
+    </div>
+</>
+
+export default Content
